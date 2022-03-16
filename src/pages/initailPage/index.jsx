64c32b35e78b51673260f803c6483aed8627011e
waleedsaifi <hafizwaleed0890@gmail.com>
2022-03-16 14:44:10 +0500

    Setup 1st API

--- src/pages/initailPage/index.jsx
+++ src/pages/initailPage/index.jsx
@@ -1,18 +1,31 @@
 import { Typography, Box, Grid } from "@mui/material";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
 import Header from "../../components/header";
 import CityCard from "../../components/cityCard";
 import "./index.css";
 import { Cities } from "../../data/data";
+const API_URL = "https://api.openweathermap.org/data/2.5";
+const CITIES_COUNT = 18;
+const APP_ID = process.env.WeatherAPI || "920ecebafee2bcc8878d5e974c9b753d";
+let city = "Lahore";
 
 export default function Index() {
-  const [cities] = React.useState(getRandomInt(Cities, 18));
+  const [citiesArray] = useState(getRandomInt(Cities, CITIES_COUNT));
+  const [selectedCity, setSelectedCity] = useState();
 
   function getRandomInt(cities, count) {
     let shuffled = cities.sort(() => 0.5 - Math.random());
     let selected = shuffled.slice(0, count);
     return selected;
   }
+
+  useEffect(() => {
+    if (selectedCity) {
+      axios.get(`${API_URL}/weather?q=${selectedCity}&appid=${APP_ID}`);
+    }
+  }, [selectedCity]);
 
   return (
     <Box>
@@ -31,7 +44,7 @@ export default function Index() {
         spacing={2}
         sx={{ padding: "20px" }}
       >
-        {cities.map((x) => (
+        {citiesArray.map((x) => (
           <Grid item xs={2}>
             <CityCard data={x} />
           </Grid>
